2c2412d12330653e33611834a5a283cf830008b3
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2025-12-15 13:32:14 +0100

    Initial Commit

---
+++ aztec-governance-widget/aztec_governance_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  aztec_governance_widgetApp.swift
+//  aztec-governance-widget
+//
+//  Created by Lasse Herskind on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct aztec_governance_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
